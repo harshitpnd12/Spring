87a2bd319d0eae7430d554a2ed2d36027fcfc0d3
harshitpnd12 <harshit200212pandey@gmail.com>
2024-10-29 12:10:11 +0530

    Something new

---
+++ src/main/java/com/springscore/lifecycle/Test.java
@@ -0,0 +1,21 @@
+package com.springscore.lifecycle;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext context=new ClassPathXmlApplicationContext("com/springscore/lifecycle/lifecycleconfig.xml");
+		context.registerShutdownHook();
+		Samosa s1=(Samosa) context.getBean("s1");
+		System.out.println(s1);
+		
+		
+		System.out.println("++++");
+		
+		Pepsi p1=(Pepsi) context.getBean("p1");
+		System.out.println(p1);
+	}
+
+}
