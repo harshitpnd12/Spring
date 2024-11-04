044038f05ed7493b8e62d4b2bd932dd15680d545
harshitpnd12 <harshit200212pandey@gmail.com>
2024-11-04 14:40:58 +0530

    practice

--- src/main/java/com/springscore/lifecycle/Test.java
+++ src/main/java/com/springscore/lifecycle/Test.java
@@ -8,14 +8,17 @@ public class Test {
 	public static void main(String[] args) {
 		AbstractApplicationContext context=new ClassPathXmlApplicationContext("com/springscore/lifecycle/lifecycleconfig.xml");
 		context.registerShutdownHook();
-		Samosa s1=(Samosa) context.getBean("s1");
-		System.out.println(s1);
+//		Samosa s1=(Samosa) context.getBean("s1");
+//		System.out.println(s1);
+//		
+//		
+//		System.out.println("++++");
+//		
+//		Pepsi p1=(Pepsi) context.getBean("p1");
+//		System.out.println(p1);
+		Example e1=(Example) context.getBean("e1");
+		System.out.println(e1);
 		
-		
-		System.out.println("++++");
-		
-		Pepsi p1=(Pepsi) context.getBean("p1");
-		System.out.println(p1);
 	}
 
 }
